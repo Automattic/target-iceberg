70711f6da0a29e5ce275f9e4fab7764e44bba3e8
Jarosław Cellary <jaroslaw.cellary@automattic.com>
2025-05-26 10:31:09 +0200

    added tests

---
+++ tests/test_sinks.py
@@ -0,0 +1,38 @@
+"""Tests standard target features using the built-in SDK tests library."""
+
+from __future__ import annotations
+
+import typing as t
+
+import pytest
+from unittest import mock
+from pyspark.sql.types import StringType
+from target_iceberg.sinks import IcebergSink
+
+TEST_CONFIG = {"table_name": "test", "column_renames": "old1=new1,old2=new2"}
+TEST_SCHEMA = {"properties": {
+        "old1": {"type": "string"},
+        "old2": {"type": "string"},
+        "Co l": {"type": "string"},
+        "col1": {"type": "string"},
+        "col2": {"type": ["null", "string"], "format": "date-time"}
+    }}
+
+def test_initialization():
+    target = mock.Mock()
+    target.config = TEST_CONFIG
+
+    sink = IcebergSink(target, TEST_SCHEMA, "test", {})
+
+    assert sink.column_renames == {'Co l': 'co_l', 'old1': 'new1', 'old2': 'new2'}
+
+
+def test_type_conversion():
+    target = mock.Mock()
+    target.config = TEST_CONFIG
+
+    sink = IcebergSink(target, TEST_SCHEMA, "test", {})
+
+    assert sink.get_spark_type(TEST_SCHEMA["properties"]["col1"]["type"]) == StringType()
+    assert sink.get_spark_type(TEST_SCHEMA["properties"]["col2"]["type"]) == StringType()
+
