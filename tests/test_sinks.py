1fd6063a6557c11ea9d9d92f4e10729b0c3e798a
Jarosław Cellary <jaroslaw.cellary@automattic.com>
2025-05-30 13:32:13 +0200

    added test

--- tests/test_sinks.py
+++ tests/test_sinks.py
@@ -3,9 +3,11 @@ from __future__ import annotations
 import typing as t
 
 import pytest
+import pyarrow as pa
 from unittest import mock
 from pyspark.sql.types import StringType, TimestampType
 from target_iceberg.sinks import IcebergSink
+from target_iceberg.utils import _field_type_to_pyarrow_field
 
 TEST_CONFIG = {"db_name": "test_db", "column_renames": "old1=new1,old2=new2"}
 TEST_SCHEMA = {"properties": {
@@ -27,3 +29,19 @@ def test_initialization():
 
 def test_to_snake_case():
     assert IcebergSink.to_snake_case("CamelCase") == "camel_case"
+
+@pytest.mark.parametrize(
+    "field_name,input_types,required_fields,expected_type,expected_nullable",
+    [
+        ("str_column", {"type": "string"}, ["str_column"], pa.string(), False),
+        ("int_column", {"type": ["null", "integer"]}, [], pa.int64(), True),
+        ("int_nullable", {"anyOf": [{"type": "integer"}, {"type": "NULL"}]}, ["int_nullable"], pa.int64(), True),
+        ("time_nullable", {"type": "string", "format": "date-time"}, ["time_nullable"], pa.timestamp("ms"), False),
+        ("str_column", {}, ["str_column"], pa.string(), False),
+    ]
+)
+def test_field_type_to_pyarrow_field(field_name, input_types, required_fields, expected_type, expected_nullable):
+    field = _field_type_to_pyarrow_field(field_name, input_types, required_fields)
+    assert field.name == field_name
+    assert field.type == expected_type
+    assert field.nullable == expected_nullable
