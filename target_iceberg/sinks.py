60b867ffe23c79eb853ea388079c445942b9f8d1
Jarosław Cellary <jaroslaw.cellary@automattic.com>
2025-06-17 10:25:18 +0200

    support for * in primary key

--- target_iceberg/sinks.py
+++ target_iceberg/sinks.py
@@ -94,6 +94,9 @@ class IcebergSink(BatchSink):
         streams = {stream[0]: IcebergSink.clean_split(stream[1], ',') for stream in streams}
         key = streams.get(self.stream_name.lower(), self.key_properties)
 
+        if key == "*":
+            key = self.pyarrow_schema.names
+
         assert set(key).issubset(set(self.pyarrow_schema.names)), \
             f"Some columns of the primary key {key} do not exist in table schema: {self.pyarrow_schema.names}"
 
